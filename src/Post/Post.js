92ade72e7a7db2663c4d26d99529f9b76e127324
Aswitha-Ajin <1112aswitha@gmail.com>
2023-02-06 06:59:54 +0530

    commit

--- src/Post/Post.js
+++ src/Post/Post.js
@@ -4,22 +4,22 @@ import "../Post/post.css"
 export default function Post() {
   return (
     <div className='postmain'>
-    
-     <img 
-     className='postimg'
-     src='https://mdbootstrap.com/img/Photos/Slides/img%20(35).webp' 
-     alt=""
-     />
-     <div className='postheading'>
-      <div className='postdetail'>
-        <span className='posttype'>Music</span>
-        <span className='posttype'>Life</span>
+
+      <img
+        className='postimg'
+        src='https://mdbootstrap.com/img/Photos/Slides/img%20(35).webp'
+        alt=""
+      />
+      <div className='postheading'>
+        <div className='postdetail'>
+          <span className='posttype'>Music</span>
+          <span className='posttype'>Life</span>
         </div>
         <span className='posttitle'>Music, art concerned with combining vocal or instrumental sounds .</span>
-         <hr/>
-         <span className='date'>1 hr ago</span>
-        </div>
-        <p className='postdata'>music, art concerned with combining vocal or instrumental sounds for beauty of form or emotional expression, usually according to cultural standards of rhythm, melody, and, in most Western music, harmony. Both the simple folk song and the complex electronic composition belong to the same activity, music.</p>
-     </div>
+        <hr />
+        <span className='date'>1 hr ago</span>
+      </div>
+      <p className='postdata'>music, art concerned with combining vocal or instrumental sounds for beauty of form or emotional expression, usually according to cultural standards of rhythm, melody, and, in most Western music, harmony. Both the simple folk song and the complex electronic composition belong to the same activity, music.</p>
+    </div>
   )
 }
